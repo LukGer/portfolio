7f0f7845c00d55f2b7be7ac67758c3212a842cff
Lukas Gerhold <lukger1999@gmail.com>
2024-12-16 13:37:50 +0100

    tailwind v4 migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [react(), mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
